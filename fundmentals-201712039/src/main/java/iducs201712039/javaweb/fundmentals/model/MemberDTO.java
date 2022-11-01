2af55e3b3316416db7e342cdd1b5c116164cd53e
Peterhabit <jiseokchoij@gmail.com>
2022-11-02 01:01:16 +0900

    feat: 데이터를 저장하는 객체(model)

---
+++ fundmentals-201712039/src/main/java/iducs201712039/javaweb/fundmentals/model/MemberDTO.java
@@ -0,0 +1,57 @@
+package iducs201712039.javaweb.fundmentals.model;
+
+import java.util.Objects;
+
+public class MemberDTO {
+    // DTO's fields
+    private String name;
+    private String email;
+    private String work;
+
+    // getter setter method : boiler plate code(상용, 관용 코드) generation
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getWork() {
+        return work;
+    }
+
+    public void setWork(String work) {
+        this.work = work;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MemberDTO memberDTO = (MemberDTO) o;
+        return Objects.equals(name, memberDTO.name) && email.equals(memberDTO.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email);
+    }
+
+    @Override
+    public String toString() { // 객체를 문자열화 할 때 사용
+        return "MemberDTO{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", work='" + work + '\'' +
+                '}';
+    }
+}
